58aba5d4127217ba3ab689beb0d32c90958bbec1
SeyedAliMousaviedu <seyedalimousaviedu@gmail.com>
2020-06-12 02:02:11 +0430

    GUI modified

--- Internet_Test.py
+++ Internet_Test.py
@@ -1,19 +1,48 @@
+import sys
 import speedtest
-s = speedtest.Speedtest()
-s.get_best_server()
-s.download()
-s.upload()
-res = s.results.dict()
-dls = int(res["download"]) * (8 ** -6)
-uls = int(res["upload"]) * (8 ** -6)
-ping = int(res["ping"])
-clientdict = res["client"]
+from tkinter import *   
+from tkinter import ttk 
 
-print ("ISP : ", clientdict["isp"])
-print ("IP : ",clientdict["ip"])
-print ("Location : ", "lat : ",clientdict["lat"]," lon : ",clientdict["lon"])
-print ("Download Speed : %5.2f MB" % dls)
-print ("Upload Speed : %5.2f MB" % uls)
-print ("Ping : %3.0f ms " % ping)
+def internet_test():    
+    s = speedtest.Speedtest()
+    s.get_best_server() 
+    s.download()    
+    s.upload()  
+    res = s.results.dict()  
+    dls = int(res["download"    ]) * (8 ** -6)
+    uls = int(res["upload"]) * (8 ** -6)
+    ping = int(res["ping"])
+    clientdict = res["client"]
+    ispstr = "ISP : " + clientdict["isp"]
+    ipstr = "IP : " + clientdict["ip"]
+    locstr = "Location : " + "lat : " + clientdict["lat"] + " lon : " + clientdict["lon"]
+    dlstr = "Download Speed : %5.2f Mb" % dls
+    ulstr = "Upload Speed : %5.2f Mb" % uls
+    pingstr = "Ping : %3.0f ms " % ping
+    return [ispstr, ipstr, locstr, dlstr, ulstr, pingstr]
 
-end = input("Please Enter any key to Exit !!!")
+def Result():
+    reswin = Toplevel()
+    reswin.title('Result',)
+    reswin.geometry('300x100')
+    reswin.resizable(False, False)
+    var = StringVar()
+    lb1 = Label(reswin, textvariable = var, relief = RAISED)
+    lb1.config(justify = 'left')
+    reinter = internet_test()
+    var.set(reinter[0] + '\n' + reinter[1] + '\n' + reinter[2] + '\n' + reinter[3] + '\n' + reinter[4] + '\n' +reinter[5])
+    lb1.pack()
+    mainloop()
+
+mainwin = Tk()
+mainwin.title('Internet Test')
+lb = Label(mainwin, text = 'Please click on Start to Run Internet Test')
+lb.pack()
+btn = Button(mainwin, text = 'Start', command = Result)
+btn.pack()
+mainwin.geometry("400x100")
+mainwin.resizable(False, False)
+lb2 = Label(mainwin, text = "By SeyedAliMousaviEdu")
+
+lb2.pack()
+mainloop()
